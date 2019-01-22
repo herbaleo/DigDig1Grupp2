e3e0245aa8a830444ea9c963494b3f82d7976668
leo lundberg <leo.lundberg@elev.ga.lbs.se>
2019-01-22 09:09:36 +0100

    Commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody2D rbody;
+    public float moveSpeed;
+    public float horizontalMovement;
+    public float jumpHeight = 6;
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+
+        rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rbody.velocity.y);
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rbody.velocity = new Vector2(rbody.velocity.x, jumpHeight);
+        }
+        
+
+        
+
+    }
+}
